9b24c522ac6ae8743fdc944719f22b70c64706c8
cosentinoandrea768-sudo <cosentinoandrea768@gmail.com>
2026-02-21 22:21:09 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    
+    message = f"""
+📈 ALERT STRATEGIA
+
+Symbol: {data.get('ticker')}
+Prezzo: {data.get('price')}
+Side: {data.get('side')}
+Timeframe: {data.get('timeframe')}
+"""
+    
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": message
+    })
+    
+    return "ok"
+
+@app.route('/')
+def home():
+    return "Bot attivo"
+
+if __name__ == "__main__":
+    app.run()
