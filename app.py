9f9694ae6666b626f2db92bd54566e040be1f311
cosentinoandrea768-sudo <cosentinoandrea768@gmail.com>
2026-02-22 02:10:28 +0100

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
+BOT_TOKEN = os.environ.get("8393500223:AAHVRqD-22xUYLwMR1wgF0VUV_u4s6U9dR0")
+CHAT_ID = os.environ.get("8369961034")
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -20,10 +20,10 @@ Side: {data.get('side')}
 Timeframe: {data.get('timeframe')}
 """
     
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot8393500223:AAHVRqD-22xUYLwMR1wgF0VUV_u4s6U9dR0/sendMessage"
     
     requests.post(url, json={
-        "chat_id": CHAT_ID,
+        "chat_id": 8369961034,
         "text": message
     })
     
